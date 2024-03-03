2ba8b605b368adf2f5d608944f24d2de23b0d142
Keith Tarco <keithtarco07@gmail.com>
2024-03-03 17:45:41 -0500

    Fin Sis Tickets

--- sockets/controller.js
+++ sockets/controller.js
@@ -1,17 +1,46 @@
+const TicketControl = require("../models/ticket-control");
 
+const ticketControl = new TicketControl();
 
 const socketController =(socket) =>{
-            
-    console.log('cliente conectado',socket.id)
-    socket.on('disconnect',()=>{
-        console.log('Cliente desconectado',socket.id);
-    });
 
-    socket.on('enviar-mensaje',( payload,callback )=>{
+    //Cliente se conecta
+    socket.emit('ultimo-ticket', ticketControl.ultimo);
+    socket.emit('estado-actual',ticketControl.ultimos4);
+    socket.emit('tickets-pendientes',ticketControl.tickets.length);
 
-        const id=12345;
-        callback(id);
-        socket.broadcast.emit('enviar-mensaje',payload.msg);
+    socket.on('siguiente-ticket',( payload,callback )=>{
+        
+        const siguiente = ticketControl.siguiente();
+        callback(siguiente);
+        socket.broadcast.emit('tickets-pendientes',ticketControl.tickets.length);
+        
+    })
+
+    socket.on('atender-ticket',({escritorio},callback)=>{
+        if(!escritorio){
+            return callback({
+                ok:false,
+                msg:'El escritorio es obligatorio.'
+            })
+        }
+        const ticket = ticketControl.atenderTicket(escritorio);
+        //Notifica cambio en los ultimos 4 
+        socket.broadcast.emit('estado-actual',ticketControl.ultimos4);
+        socket.emit('tickets-pendientes',ticketControl.tickets.length);
+        socket.broadcast.emit('tickets-pendientes',ticketControl.tickets.length);
+
+        if(!ticket){
+            callback({
+                ok:false,
+                msg:'Ya no hay tickets pendientes'
+            })
+        }else{
+            callback({
+                ok:true,
+                ticket
+            })
+        }
     })
 
 }
